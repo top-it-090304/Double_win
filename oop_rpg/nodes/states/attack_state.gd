86ee5c1b49b05b912be4803217a661fcda04331a
Marsel <marselshkl2006@gmail.com>
2026-02-08 23:45:02 +0600

    Delete oop_rpg directory

--- oop_rpg/nodes/states/attack_state.gd
+++
@@ -1,7 +0,0 @@
-class_name AttackState
-extends State
-
-func enter() -> void:
-    character.animation_player.play("attack")
-    await character.animation_player.animation_finished
-    character.state_machine.transition_to("Idle")
--- oop_rpg/nodes/states/idle_state.gd
+++
@@ -1,6 +0,0 @@
-class_name IdleState
-extends State
-
-func enter() -> void:
-    character.animation_player.play("idle")
-    character.velocity = Vector2.ZERO
