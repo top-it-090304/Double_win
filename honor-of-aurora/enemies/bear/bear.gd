7443066878cf30952d64dd441c595a8f1dc24fbe
Toly-Leto <anatolii_stankovskiii@mail.ru>
2026-02-26 10:52:54 +0600

    Добавил всем врагам собственный скрипт с параметрами. Параметры нужно отрегулировать

---
+++ honor-of-aurora/enemies/bear/bear.gd
@@ -0,0 +1,12 @@
+extends "res://enemies/enemy_base.gd"
+
+func _ready():
+	super()  
+	
+   
+	speed = 150.0  
+	health = 200   
+	attack_damage = 35 
+	gold_reward = 200 
+	detection_radius = 700.0  
+	attack_radius = 200.0
