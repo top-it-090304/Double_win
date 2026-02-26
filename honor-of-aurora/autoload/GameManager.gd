85ed4ea32b915661eae96177c571e993d7b57e9b
Toly-Leto <anatolii_stankovskiii@mail.ru>
2026-02-27 00:53:49 +0600

    Добавил переход от главного меню в основную сцену игры на базу по нажатию на кнопку

--- honor-of-aurora/autoload/GameManager.gd
+++ honor-of-aurora/autoload/GameManager.gd
@@ -1,13 +1,26 @@
 extends Node
 
+func _ready() -> void:
+	Events.location_changed.connect(handle_location_changed)
+
 var gold: int = 0
+const location_to_scene = {
+	Events.LOCATION.BASE: preload("res://world/islads/base/base_islad.tscn"), 
+	Events.LOCATION.LVL1: preload("res://world/islads/levels/level_1.tscn"), 
+	Events.LOCATION.LVL2: preload("res://world/islads/levels/level_2.tscn"), 
+	Events.LOCATION.LVL3: preload("res://world/islads/levels/level_3.tscn"), 
+	Events.LOCATION.LVL4: preload("res://world/islads/levels/level_4.tscn"), 
+	Events.LOCATION.LVL5: preload("res://world/islads/levels/level_5.tscn"), 
+	
+}
 
 func handle_location_changed(location: Events.LOCATION):
-	pass
+	get_tree().change_scene_to_packed(location_to_scene.get(location))
 	
 func add_gold(amount: int):
 	gold += amount
 	Events.gold_changed.emit(gold)
+	
 	
 	
 	
