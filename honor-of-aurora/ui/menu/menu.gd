00e83e5202ef912e2802b4a5be096ca94e9ffc46
Toly-Leto <anatolii_stankovskiii@mail.ru>
2026-02-28 20:42:08 +0600

    Улучшил сцетовую схему в UI для лучшего восприятия, заменил рабочие названия островов в меню, на конечные названия

--- honor-of-aurora/ui/menu/menu.gd
+++ honor-of-aurora/ui/menu/menu.gd
@@ -3,3 +3,15 @@ extends Control
 
 func _on_new_game_pressed() -> void:
 	Events.location_changed.emit(Events.LOCATION.BASE)
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
