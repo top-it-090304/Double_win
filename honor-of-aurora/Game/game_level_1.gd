2028ab5372b64dc95d26b445c1ec9865ac7931d4
Toly-Leto <anatolii_stankovskiii@mail.ru>
2026-03-05 16:47:59 +0600

    Добавил спавн врагов на первом уровне

--- honor-of-aurora/Game/game_level_1.gd
+++ honor-of-aurora/Game/game_level_1.gd
@@ -1,14 +1,47 @@
-extends TileMapLayer
-
+extends Node2D
 
 const spawn_position = Vector2(600, 740)
-func _ready() -> void:
-	pass 
 
+@export var enemy_scenes: Array[PackedScene] = [
+	preload("res://enemies/snake/Snake.tscn"),
+	preload("res://enemies/gnome/gnome.tscn")
+]
 
-func _process(delta: float) -> void:
-	pass
+@export var spawn_points: Array[Marker2D] = []
+@export var max_enemies: int = 5
+@export var random_spawn: bool = true
 
-func add_player_on_sscene(player: Node2D):
-	self.add_child(player)
+var current_index: int = 0
+
+func _ready():
 	
+	if SaveManager.boss_kill < 1:
+		spawn_enemies()
+	else:
+		queue_free()
+
+func spawn_enemies():
+	if spawn_points.is_empty():
+		return
+	
+	if enemy_scenes.is_empty():
+		return
+	
+	var spawned_count = 0
+	for point in spawn_points:
+		if spawned_count >= max_enemies:
+			break
+			
+		var enemy_scene = get_next_enemy_scene()
+		var enemy = enemy_scene.instantiate()
+		enemy.global_position = point.global_position
+		add_child(enemy)
+		spawned_count += 1
+
+func get_next_enemy_scene() -> PackedScene:
+	if random_spawn:
+		return enemy_scenes[randi() % enemy_scenes.size()]
+	else:
+		var scene = enemy_scenes[current_index]
+		current_index = (current_index + 1) % enemy_scenes.size()
+		return scene
